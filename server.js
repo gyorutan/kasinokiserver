e820dfde236a7cc17c6361f6d86edb9a512e1c40
gyorutan <127669165+gyorutan@users.noreply.github.com>
2023-04-04 18:15:19 +0900

    Update server.js

--- server.js
+++ server.js
@@ -105,11 +105,12 @@ app.post("/create", async (req, res) => {
   const { userId, title, content } = req.body;
 
   const date = new Date();
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  const hour = String(date.getHours()).padStart(2, "0");
-  const minute = String(date.getMinutes()).padStart(2, "0");
+  const newDate = new Date(date.getTime() + 9 * 60 * 60 * 1000);
+  const year = newDate.getFullYear();
+  const month = String(newDate.getMonth() + 1).padStart(2, "0");
+  const day = String(newDate.getDate()).padStart(2, "0");
+  const hour = String(newDate.getHours()).padStart(2, "0");
+  const minute = String(newDate.getMinutes()).padStart(2, "0");
 
   try {
     const findUser = await User.findOne({ _id: userId }).exec();
